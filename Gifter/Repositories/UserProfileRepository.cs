3f5d80785e9298495031f8b0099ee0d7951859a1
David Larsen <davidplarsen@gmail.com>
2020-09-30 11:48:28 -0500

    Complete exercise 2

---
+++ Gifter/Repositories/UserProfileRepository.cs
@@ -0,0 +1,51 @@
+﻿using Gifter.Models;
+using Gifter.Utils;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Repositories
+{
+    public class UserProfileRepository : BaseRepository
+    {
+        public UserProfileRepository(IConfiguration configuration) : base(configuration) {}
+
+        public List<UserProfile> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                SELECT [Name], Email, ImageUrl, Bio, DateCreated
+                  FROM UserProfile p 
+              ORDER BY p.DateCreated";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var posts = new List<UserProfile>();
+                    while (reader.Read())
+                    {
+                        posts.Add(new UserProfile()
+                        {
+                            Id = DbUtils.GetInt(reader, "PostId"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            Bio = DbUtils.GetString(reader, "Bio"),
+                            ImageUrl = DbUtils.GetString(reader, "ImageUrl"),
+                            DateCreated = DbUtils.GetDateTime(reader, "DateCreated")
+                        });
+                    }
+
+                    reader.Close();
+
+                    return posts;
+                }
+            }
+        }
+
+    }
+}
